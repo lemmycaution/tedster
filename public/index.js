fb53a3dab44e1726473d64348fdf261aa7534cbd
Onur Uyar <me@onuruyar.com>
2015-01-15 22:27:45 +0000

    small fixes and improvements

--- public/index.js
+++ public/index.js
@@ -1,33 +1,42 @@
 (function(){
   
   var 
+    SIZE = 120,
+    PLACEHOLDER = "Enter a word containing 'ted'",
     MAX_TITLE_LENGTH = 15,
     TED='<span class="red">TED</span>',
     CURSOR = '<span class="cursor"></span>',
+    pat = new RegExp(".*(ted$)", "i")
     images = ["destr.jpg","homs.jpg","homs2.jpg","hote.jpg","imaginestreet.jpg"],
     image = null,
     inputEl = document.getElementsByName("title")[0],
     viewEl = document.getElementsByClassName("title")[0],
     bgEl = document.getElementsByClassName("bg")[0],
     downloadEl = document.getElementsByClassName("download")[0],
+    downloadLinkEl = document.getElementsByClassName("download-link")[0],  
     title = null;
   
   inputEl.addEventListener("keyup", function(e) {
+    if(e.keyCode===37){
+      e.preventDefault()
+      inputEl.value = inputEl.value.substr(0,inputEl.value.length-1)
+    }
     // all other letters become lowercase, TED capitalised
     title = (e.target.value || "").toLowerCase();
 
     // add any word less than 15 characters(?) that contains the letters 'ted' in a sequence
-    if (title.length < MAX_TITLE_LENGTH && title.match(/ted$/) !== null) {
+    if (title.length <= MAX_TITLE_LENGTH && title.match(pat) !== null) {
       image = images[Math.floor(Math.random()*images.length)];
 
       downloadEl.setAttribute("style", "display:block");
-      downloadEl.setAttribute("href", "/image?text=" + 
-        encodeURIComponent(title.replace(/ted$/i,"TED")) + 
-        "&image=" + encodeURIComponent(image)
+      downloadLinkEl.setAttribute("href", "/image?text=" + 
+        encodeURIComponent(title.replace(/ted$/i,"")) + 
+        "&image=" + encodeURIComponent(image) + 
+        "&size=" + SIZE
       );
       
       image = "url(/images/"+image+")";      
-      title = title.replace(/ted$/i,TED);      
+      title = title.replace(/ted$/i,"") + title.replace(pat,TED);      
     }else{
       image = "none";
       downloadEl.removeAttribute("style");      
@@ -36,7 +45,7 @@
     bgEl.setAttribute("style","background-image:" + image);    
     
     if (title === ""){
-      title = "How do you feel?"
+      title = PLACEHOLDER
     }
     
     viewEl.innerHTML = title + CURSOR;
